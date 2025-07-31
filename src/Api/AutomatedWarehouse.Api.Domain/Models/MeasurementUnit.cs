db0fc5603d7139a80fdfd31b5d7feabef1dadce0
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-07-31 17:18:07 +0400

    Add IsArchived default value false

--- src/Api/AutomatedWarehouse.Api.Domain/Models/MeasurementUnit.cs
+++ src/Api/AutomatedWarehouse.Api.Domain/Models/MeasurementUnit.cs
@@ -4,6 +4,6 @@
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
-        public bool IsArchived { get; set; }
+        public bool IsArchived { get; set; } = false;
     }
 }
