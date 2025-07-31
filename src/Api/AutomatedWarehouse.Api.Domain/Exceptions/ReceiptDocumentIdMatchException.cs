f426e707f2a26c6e724779dfec7301d3fc0fa135
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-07-31 17:18:25 +0400

    Add custom exception

---
+++ src/Api/AutomatedWarehouse.Api.Domain/Exceptions/ReceiptDocumentIdMatchException.cs
@@ -0,0 +1,8 @@
+﻿namespace AutomatedWarehouse.Api.Domain.Exceptions
+{
+    public class ReceiptDocumentIdMatchException : Exception
+    {
+        public ReceiptDocumentIdMatchException(string message) : base(message)
+        { }
+    }
+}
