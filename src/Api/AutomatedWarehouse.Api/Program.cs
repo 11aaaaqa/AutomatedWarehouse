7a50a448ada3eeb0f440074690f049fbbf903192
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-07-31 17:17:53 +0400

    Register services and database

--- src/Api/AutomatedWarehouse.Api/Program.cs
+++ src/Api/AutomatedWarehouse.Api/Program.cs
@@ -1,4 +1,19 @@
+using AutomatedWarehouse.Api.Domain.Models;
+using AutomatedWarehouse.Api.Infrastructure.Database;
+using AutomatedWarehouse.Api.Infrastructure.Services.Guide_services;
+using AutomatedWarehouse.Api.Infrastructure.Services.Receipt_services.Document_services;
+using AutomatedWarehouse.Api.Infrastructure.Services.Receipt_services.Resource_services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>(x
+    => x.UseNpgsql(builder.Configuration["Database:ConnectionString"]));
+
+builder.Services.AddTransient<IGuideService<Resource>, ResourceService>();
+builder.Services.AddTransient<IGuideService<MeasurementUnit>, MeasurementService>();
+builder.Services.AddTransient<IReceiptResourceService, ReceiptResourceService>();
+builder.Services.AddTransient<IReceiptDocumentService, ReceiptDocumentService>();
 
 builder.Services.AddControllers();
 
