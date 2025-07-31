37e51099d7ccec1351103b0310a3722a6d3446bc
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-07-31 17:42:52 +0400

    Add controllers

---
+++ src/Api/AutomatedWarehouse.Api/Controllers/MeasurementUnitController.cs
@@ -0,0 +1,70 @@
+﻿using AutomatedWarehouse.Api.Domain.Models;
+using AutomatedWarehouse.Api.DTOs.Measurement_unit;
+using AutomatedWarehouse.Api.Infrastructure.Services.Guide_services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutomatedWarehouse.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MeasurementUnitController(IGuideService<MeasurementUnit> measurementUnitService) : ControllerBase
+    {
+        [HttpGet]
+        [Route("GetById/{measurementUnitId}")]
+        public async Task<IActionResult> GetByIdAsync(Guid measurementUnitId)
+            => Ok(await measurementUnitService.GetByIdAsync(measurementUnitId));
+
+        [HttpGet]
+        [Route("GetAll")]
+        public async Task<IActionResult> GetAllArchivedAsync(bool isArchived)
+            => Ok(await measurementUnitService.GetAllAsync(isArchived));
+
+        [HttpDelete]
+        [Route("Delete/{measurementUnitId}")]
+        public async Task<IActionResult> DeleteAsync(Guid measurementUnitId)
+        {
+            try
+            {
+                await measurementUnitService.DeleteAsync(measurementUnitId);
+            }
+            catch (InvalidOperationException)
+            {
+                return Conflict("Measurement unit cannot be deleted because it is used in another table");
+            }
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("Create")]
+        public async Task<IActionResult> CreateAsync([FromBody] CreateMeasurementUnitDto model)
+        {
+            try
+            {
+                await measurementUnitService.CreateAsync(new MeasurementUnit
+                    { Id = Guid.NewGuid(), Name = model.Name });
+            }
+            catch (InvalidOperationException)
+            {
+                return Conflict("Measurement unit with current name already exists");
+            }
+
+            return Ok();
+        }
+
+        [HttpPut]
+        [Route("Update")]
+        public async Task<IActionResult> UpdateAsync([FromBody] MeasurementUnit model)
+        {
+            try
+            {
+                await measurementUnitService.UpdateAsync(model);
+            }
+            catch (InvalidOperationException)
+            {
+                return Conflict("Measurement unit with current name already exists");
+            }
+
+            return Ok();
+        }
+    }
+}
---
+++ src/Api/AutomatedWarehouse.Api/Controllers/ResourceController.cs
@@ -0,0 +1,70 @@
+﻿using AutomatedWarehouse.Api.Domain.Models;
+using AutomatedWarehouse.Api.DTOs.Resource;
+using AutomatedWarehouse.Api.Infrastructure.Services.Guide_services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutomatedWarehouse.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ResourceController(IGuideService<Resource> resourceService) : ControllerBase
+    {
+        [HttpGet]
+        [Route("GetById/{resourceId}")]
+        public async Task<IActionResult> GetByIdAsync(Guid resourceId)
+            => Ok(await resourceService.GetByIdAsync(resourceId));
+
+        [HttpGet]
+        [Route("GetAll")]
+        public async Task<IActionResult> GetAllAsync(bool isArchived)
+            => Ok(await resourceService.GetAllAsync(isArchived));
+
+        [HttpDelete]
+        [Route("Delete/{resourceId}")]
+        public async Task<IActionResult> DeleteAsync(Guid resourceId)
+        {
+            try
+            {
+                await resourceService.DeleteAsync(resourceId);
+            }
+            catch (InvalidOperationException)
+            {
+                return Conflict("Resource cannot be deleted because it is used in another table");
+            }
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("Create")]
+        public async Task<IActionResult> CreateAsync([FromBody] CreateResourceDto model)
+        {
+            try
+            {
+                await resourceService.CreateAsync(new Resource
+                    { Id = Guid.NewGuid(), Name = model.Name });
+            }
+            catch (InvalidOperationException)
+            {
+                return Conflict("Resource with current name already exists");
+            }
+
+            return Ok();
+        }
+
+        [HttpPut]
+        [Route("Update")]
+        public async Task<IActionResult> UpdateAsync([FromBody] Resource model)
+        {
+            try
+            {
+                await resourceService.UpdateAsync(model);
+            }
+            catch (InvalidOperationException)
+            {
+                return Conflict("Resource with current name already exists");
+            }
+
+            return Ok();
+        }
+    }
+}
