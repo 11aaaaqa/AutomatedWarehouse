a86f1d9b74adfe967c3aa40360ae1d16096931b1
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-07-31 17:17:34 +0400

    Add database context

---
+++ src/Api/AutomatedWarehouse.Api.Infrastructure/Database/ApplicationDbContext.cs
@@ -0,0 +1,41 @@
+﻿using AutomatedWarehouse.Api.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AutomatedWarehouse.Api.Infrastructure.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Resource> Resources { get; set; }
+        public DbSet<MeasurementUnit> MeasurementUnits { get; set; }
+        public DbSet<ReceiptDocument> ReceiptDocuments { get; set; }
+        public DbSet<ReceiptResource> ReceiptResources { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<ReceiptDocument>()
+                .HasMany<ReceiptResource>()
+                .WithOne()
+                .HasForeignKey(x => x.ReceiptDocumentId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<MeasurementUnit>()
+                .HasMany<ReceiptResource>()
+                .WithOne()
+                .HasForeignKey(x => x.MeasurementUnitId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Resource>()
+                .HasMany<ReceiptResource>()
+                .WithOne()
+                .HasForeignKey(x => x.ResourceId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Resource>().HasIndex(x => x.Name).IsUnique();
+            builder.Entity<MeasurementUnit>().HasIndex(x => x.Name).IsUnique();
+            builder.Entity<ReceiptDocument>().HasIndex(x => x.ReceiptNumber).IsUnique();
+        }
+    }
+}
