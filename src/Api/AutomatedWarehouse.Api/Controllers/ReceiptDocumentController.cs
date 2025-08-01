d62431e35a6ca811341cbf30862487cc7c1112c5
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-08-01 10:22:05 +0400

    Add receipt document controller

---
+++ src/Api/AutomatedWarehouse.Api/Controllers/ReceiptDocumentController.cs
@@ -0,0 +1,95 @@
+﻿using AutomatedWarehouse.Api.Domain.Exceptions;
+using AutomatedWarehouse.Api.Domain.Models;
+using AutomatedWarehouse.Api.DTOs.Receipt;
+using AutomatedWarehouse.Api.Infrastructure.Services.Receipt_services.Document_services;
+using AutomatedWarehouse.Api.Infrastructure.Services.Receipt_services.Resource_services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutomatedWarehouse.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReceiptDocumentController(IReceiptDocumentService receiptDocumentService,
+        IReceiptResourceService receiptResourceService) : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync([FromBody] CreateReceiptDocumentDto model)
+        {
+            try
+            {
+                await receiptDocumentService.AddAsync(new ReceiptDocument
+                {
+                    Id = model.ReceiptDocumentId,
+                    ReceiptDate = DateOnly.FromDateTime(DateTime.UtcNow),
+                    ReceiptNumber = model.ReceiptNumber
+                });
+                await receiptResourceService.UpdateReceiptDocumentResourcesAsync(model.ReceiptResources, model.ReceiptDocumentId);
+            }
+            catch (InvalidOperationException)
+            {
+                return Conflict("Receipt document with current receipt number already exists");
+            }
+            catch (ReceiptDocumentIdMatchException ex)
+            {
+                await receiptDocumentService.DeleteAsync(model.ReceiptDocumentId);
+                return BadRequest(ex.Message);
+            }
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateAsync([FromBody] UpdateReceiptDocumentDto model)
+        {
+            try
+            {
+                await receiptDocumentService.UpdateAsync(new ReceiptDocument
+                {
+                    Id = model.ReceiptDocumentId, ReceiptDate = model.ReceiptDate, ReceiptNumber = model.ReceiptNumber
+                });
+                await receiptResourceService.UpdateReceiptDocumentResourcesAsync(model.ReceiptResources,
+                    model.ReceiptDocumentId);
+            }
+            catch (InvalidOperationException)
+            {
+                return Conflict("Receipt document with current receipt number already exists");
+            }
+            catch (ReceiptDocumentIdMatchException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteAsync(Guid receiptDocumentId)
+        {
+            await receiptDocumentService.DeleteAsync(receiptDocumentId);
+
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("GetFilteredReceiptDocuments")]
+        public async Task<IActionResult> GetFilteredReceiptDocumentsAsync([FromBody] GetFilteredReceiptDocumentsDto model)
+        {
+            var receiptDocuments = await receiptDocumentService.GetReceiptDocumentsAsync(
+                model.DateFrom, model.DateUntil, model.ReceiptNumbers, model.ResourceIds, model.MeasurementUnitIds);
+            return Ok(receiptDocuments);
+        }
+
+        [HttpGet]
+        [Route("GetReceiptDocumentNumbers")]
+        public async Task<IActionResult> GetReceiptDocumentNumbersAsync()
+        {
+            return Ok(await receiptDocumentService.GetReceiptDocumentNumbersAsync());
+        }
+
+        [HttpGet]
+        [Route("{receiptDocumentId}")]
+        public async Task<IActionResult> GetReceiptResourcesByDocumentAsync(Guid receiptDocumentId)
+        {
+            return Ok(await receiptDocumentService.GetByIdAsync(receiptDocumentId));
+        }
+    }
+}
