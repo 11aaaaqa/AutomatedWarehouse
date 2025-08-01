92ac9aa4e5cc5201c52b17cf30e93412adfc3603
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-08-01 10:31:16 +0400

    Add initial mvc project

---
+++ src/MVC/AutomatedWarehouse.MVC/Program.cs
@@ -0,0 +1,22 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHttpClient();
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseRouting();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+
+app.Run();
