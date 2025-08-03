0584c3a8d454ef4454ef86442c2e4c229c2cb3a4
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-08-03 14:07:36 +0400

    Add view model

---
+++ src/MVC/AutomatedWarehouse.MVC/Models/View models/AddReceiptDocumentViewModel.cs
@@ -0,0 +1,10 @@
+﻿using AutomatedWarehouse.MVC.Response_models.Resource;
+
+namespace AutomatedWarehouse.MVC.Models.View_models
+{
+    public class AddReceiptDocumentViewModel
+    {
+        public List<ResourceResponseModel> Resources { get; set; }
+        public List<MeasurementUnitResponseModel> MeasurementUnits { get; set; }
+    }
+}
