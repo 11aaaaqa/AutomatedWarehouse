bbd817657f0b79b3596df579e6bc863c6e24ca70
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-08-03 14:06:28 +0400

    Add DTOs

---
+++ src/MVC/AutomatedWarehouse.MVC/DTOs/AddReceiptDto.cs
@@ -0,0 +1,9 @@
+﻿namespace AutomatedWarehouse.MVC.DTOs
+{
+    public class AddReceiptDto
+    {
+        public uint Number { get; set; }
+        public DateOnly Date { get; set; }
+        public List<AddReceiptReceiptResourceDto> ReceiptResources { get; set; } = new();
+    }
+}
---
+++ src/MVC/AutomatedWarehouse.MVC/DTOs/AddReceiptReceiptResourceDto.cs
@@ -0,0 +1,9 @@
+﻿namespace AutomatedWarehouse.MVC.DTOs
+{
+    public class AddReceiptReceiptResourceDto
+    {
+        public Guid ResourceId { get; set; }
+        public Guid MeasurementUnitId { get; set; }
+        public uint Quantity { get; set; }
+    }
+}
