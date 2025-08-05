841ddcbeefe06aff1727ac2410291df804fdd731
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-08-05 11:49:54 +0400

    Add date from and date until to filter

--- src/MVC/AutomatedWarehouse.MVC/DTOs/GetFilteredReceiptsDto.cs
+++ src/MVC/AutomatedWarehouse.MVC/DTOs/GetFilteredReceiptsDto.cs
@@ -2,6 +2,8 @@
 {
     public class GetFilteredReceiptsDto
     {
+        public DateOnly DateFrom { get; set; }
+        public DateOnly DateUntil { get; set; }
         public List<string> ReceiptNumbers { get; set; } = new();
         public List<Guid> ResourceIds { get; set; } = new();
         public List<Guid> MeasurementUnitIds { get; set; } = new();
