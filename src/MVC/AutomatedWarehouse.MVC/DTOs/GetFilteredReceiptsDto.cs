ea37b37cf70a6953eb5ebcdc955f1a1570d7df87
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-08-05 11:17:15 +0400

    Add dto

---
+++ src/MVC/AutomatedWarehouse.MVC/DTOs/GetFilteredReceiptsDto.cs
@@ -0,0 +1,9 @@
+﻿namespace AutomatedWarehouse.MVC.DTOs
+{
+    public class GetFilteredReceiptsDto
+    {
+        public List<string> ReceiptNumbers { get; set; } = new();
+        public List<Guid> ResourceIds { get; set; } = new();
+        public List<Guid> MeasurementUnitIds { get; set; } = new();
+    }
+}
