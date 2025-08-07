d003ce4ba4f438e2aa5641e128dfee67b3fe41fe
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-08-07 15:46:08 +0400

    Add automatic database creating if doesnt exist

--- src/Api/AutomatedWarehouse.Api.Infrastructure/Database/ApplicationDbContext.cs
+++ src/Api/AutomatedWarehouse.Api.Infrastructure/Database/ApplicationDbContext.cs
@@ -1,11 +1,33 @@
 ﻿using AutomatedWarehouse.Api.Domain.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
 
 namespace AutomatedWarehouse.Api.Infrastructure.Database
 {
     public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContextOptions options) : base(options) { }
+        public ApplicationDbContext(DbContextOptions options, IConfiguration configuration) : base(options)
+        {
+            try
+            {
+                Database.OpenConnection();
+            }
+            catch (PostgresException ex) when(ex.SqlState == "3D000")
+            {
+                var masterConnectionString = new NpgsqlConnectionStringBuilder(configuration["Database:ConnectionString"])
+                {
+                    Database = "postgres"
+                }.ToString();
+
+                using var conn = new NpgsqlConnection(masterConnectionString);
+                conn.Open();
+                using var cmd = new NpgsqlCommand($"CREATE DATABASE \"{Database.GetDbConnection().Database}\" ENCODING 'UTF8' LC_COLLATE 'en_US.UTF-8' LC_CTYPE 'en_US.UTF-8' TEMPLATE template0", conn);
+                cmd.ExecuteNonQuery();
+
+                Database.Migrate();
+            }
+        }
 
         public DbSet<Resource> Resources { get; set; }
         public DbSet<MeasurementUnit> MeasurementUnits { get; set; }
