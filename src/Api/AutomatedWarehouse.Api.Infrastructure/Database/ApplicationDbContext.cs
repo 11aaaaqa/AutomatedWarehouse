60c272d1c9e2f22d636b9e22c7b6f34499ef0a2c
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-08-07 12:21:06 +0400

    Add ensure created instead of migrations

--- src/Api/AutomatedWarehouse.Api.Infrastructure/Database/ApplicationDbContext.cs
+++ src/Api/AutomatedWarehouse.Api.Infrastructure/Database/ApplicationDbContext.cs
@@ -1,11 +1,28 @@
 ﻿using AutomatedWarehouse.Api.Domain.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
 
 namespace AutomatedWarehouse.Api.Infrastructure.Database
 {
     public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContextOptions options) : base(options) { }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IConfiguration configuration) : base(options)
+        {
+            if (!Database.CanConnect())
+            {
+                var masterConnectionString = new NpgsqlConnectionStringBuilder(configuration["Database:ConnectionString"])
+                {
+                    Database = "postgres" 
+                }.ToString();
+
+                using var conn = new NpgsqlConnection(masterConnectionString);
+                conn.Open();
+                using var cmd = new NpgsqlCommand($"CREATE DATABASE \"{Database.GetDbConnection().Database}\" ENCODING 'UTF8' LC_COLLATE 'English_United States.1252' LC_CTYPE 'English_United States.1252' TEMPLATE template0", conn);
+                cmd.ExecuteNonQuery();
+            }
+            Database.EnsureCreated();
+        }
 
         public DbSet<Resource> Resources { get; set; }
         public DbSet<MeasurementUnit> MeasurementUnits { get; set; }
